408aae8c09e837278465ba5b45072e7e8d49311a
Michael Boulton <boulton@zoetrope.io>
2017-08-24 09:40:46 +0100

    Move block of code out of try/catch block

--- mongoengine/base/common.py
+++ mongoengine/base/common.py
@@ -21,20 +21,20 @@ def get_document(name):
         possible_match = [k for k in _document_registry.keys()
                           if k.endswith(compound_end) or k == single_end]
         if possible_match:
+            # Get them both as a set of everything they inherit from
+            inherited = name.split(".")
+            possible_inherited = [i.split(".") for i in possible_match]
+
+            set_inh = set(inherited)
+
+            # first try to get an exact match
+            same_inheritance = [i for i in possible_inherited if set(i) <= set_inh and set_inh <= set(i)]
+
+            # If there isn't an exact match, just get one that is 'close'
+            if not same_inheritance:
+                same_inheritance = sorted([i for i in possible_inherited if set(i) >= set_inh], key=len)
+
             try:
-                # Get them both as a set of everything they inherit from
-                inherited = name.split(".")
-                possible_inherited = [i.split(".") for i in possible_match]
-
-                set_inh = set(inherited)
-
-                # first try to get an exact match
-                same_inheritance = [i for i in possible_inherited if set(i) <= set_inh and set_inh <= set(i)]
-
-                # If there isn't an exact match, just get one that is 'close'
-                if not same_inheritance:
-                    same_inheritance = sorted([i for i in possible_inherited if set(i) >= set_inh], key=len)
-
                 doc = _document_registry.get(".".join(same_inheritance[0]), None)
             except IndexError:
                 doc = None
